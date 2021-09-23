2b98a5cb81ced742ea01a1a8312eafb2e664f926
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-09-23 16:29:27 -0600

    started over with code

--- robot/assets/js/game.js
+++ robot/assets/js/game.js
@@ -1,36 +1,20 @@
-// Game States
-// "WIN" - Player robot has defeated all enemy-robots
-//    * Fight all enemy-robots
-//    * Defeat each enemy-robot
-// "LOSE" - Player robot's health is zero or less
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
-var playerMoney = 10;
 
-var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
-console.log(enemyNames);
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-console.log(enemyNames.length);
-
-for (var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
-  console.log(i);
-  console.log(enemyNames[i] + " is at " + i + " index");
-}
-//alert players that they are starting the round
-window.alert("Welcome to Robot Gladiators!");
-var promptFight = window.prompt(
-  "Would  you like to FIGHT OR SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
-);
-// if player choses to fight, then fight
-if (promptFight === "fight" || promptFight === "FIGHT") {
-  // remove enemy's health by subtracting the amount set in the playerAttack variable
+var fight = function () {
+  // Alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
   enemyHealth = enemyHealth - playerAttack;
+
+  // Log a resulting message to the console so we know that it worked.
   console.log(
     playerName +
       " attacked " +
@@ -41,46 +25,31 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
       enemyHealth +
       " health remaining."
   );
-} else if (promptFight === "skip" || promptFight === "SKIP") {
-  // confirm player wants to skip
-  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-  //if yes (true), leave fight
-  if (confirmSkip) {
-    window.alert((playerName = " has decided to skip this fight. Goodbye!"));
-    // subtract money from playerMoney for skipping
-    playerMoney = playerMoney - 2;
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
   }
-  // if no (false), ask question again by running fight() again
-  else {
-    //fight();
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
+  playerHealth = playerHealth - enemyAttack;
+
+  // Log a resulting message to the console so we know that it worked.
+  console.log(
+    enemyName +
+      " attacked " +
+      playerName +
+      ". " +
+      playerName +
+      " now has " +
+      playerHealth +
+      " health remaining."
+  );
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
   }
-}
-
-// check enemy's health
-if (enemyHealth <= 0) {
-  window.alert(enemyName + " has died!");
-} else {
-  window.alert(enemyName + " still has " + enemyHealth + " health left.");
-}
-
-// remove player's health by subtracting the amount set in the enemyAttack variable
-playerHealth = playerHealth - enemyAttack;
-console.log(
-  enemyName +
-    " attacked " +
-    playerName +
-    ". " +
-    playerName +
-    " now has " +
-    playerHealth +
-    " health remaining."
-);
-
-// check player's health
-if (playerHealth <= 0) {
-  window.alert(playerName + " has died!");
-} else {
-  window.alert(playerName + " still has " + playerHealth + " health left.");
-}
-// if player choses to skip
+};
+fight();
